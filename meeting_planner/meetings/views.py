ca46f255f74e9e57c94ab301f5590ad07f8c4547
Mohsen <mohsen.asadipour@algorithmx.com>
2022-09-06 06:29:53 +0000

    a template with a ModelForm

--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Meeting, Room
-
+from django.forms import modelform_factory
 
 def detail(request, id):
     #meeting = Meeting.objects.get(pk=id)
@@ -24,4 +24,10 @@ def rooms(request):
 # - template
 # - url mapping
 
+
+MeetingForm = modelform_factory(Meeting, exclude=[])
+
+def new(request):
+    form = MeetingForm()
+    return render(request, "meetings/new.html", {"form": form})
 
