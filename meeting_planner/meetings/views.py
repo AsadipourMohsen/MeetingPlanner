85280445391243d98d47d72a467b6d0ffc287b3d
Mohsen <mohsen.asadipour@algorithmx.com>
2022-09-06 16:34:25 +0000

    writing to the database

--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Meeting, Room
 from django.forms import modelform_factory
 
@@ -28,6 +28,13 @@ def rooms(request):
 MeetingForm = modelform_factory(Meeting, exclude=[])
 
 def new(request):
-    form = MeetingForm()
+    if request.method == "POST":
+        #form has been submitted, process data
+        form = MeetingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("welcome")
+    else:
+        form = MeetingForm()
     return render(request, "meetings/new.html", {"form": form})
 
